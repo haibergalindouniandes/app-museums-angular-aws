ec206b8fc54610018631f1eec7730f66829f837b
Haiber H. Galindo <h.galindos@uniandes.edu.co>
2022-12-30 15:29:57 -0500

    Proyecto inicial Angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+const baseUrl = 'http://ec2-44-203-154-12.compute-1.amazonaws.com:3000/aws/cloudapp/v1/';
+const privateKeyApiMuseums = '16b8318a-864b-11ed-a1eb-0242ac120002';
+const userApi = 'zlaifer619';
+const passwordApi = 'Zl@ifer619';
+
 export const environment = {
-  production: true
+  production: true,
+  baseUrl,
+  privateKeyApiMuseums,
+  userApi,
+  passwordApi,
 };
